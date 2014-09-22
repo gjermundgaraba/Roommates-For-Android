e941a2f931930cef19a5a914c0a5ede4783436af
Gjermund Bjaanes <bjaanes@gmail.com>
2014-09-22 21:05:10 +0200

    General clean up. Pulled out some string values.

--- app/src/main/java/com/realkode/roomates/Feed/FeedFragment.java
+++ app/src/main/java/com/realkode/roomates/Feed/FeedFragment.java
@@ -68,9 +68,9 @@ public class FeedFragment extends Fragment implements RefreshableFragment {
 
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
 
-        alertDialogBuilder.setTitle(context.getString(R.string.title_create_new_note))
-                .setPositiveButton(context.getString(R.string.button_add_note), new AddNoteOnClickListener(noteEditText, noteAdapter, context))
-                .setNegativeButton(context.getString(R.string.button_cancel), null)
+        alertDialogBuilder.setTitle(R.string.title_create_new_note)
+                .setPositiveButton(R.string.button_add_note, new AddNoteOnClickListener(noteEditText, noteAdapter, context))
+                .setNegativeButton(R.string.button_cancel, null)
                 .setView(noteDialogView);
 
 
