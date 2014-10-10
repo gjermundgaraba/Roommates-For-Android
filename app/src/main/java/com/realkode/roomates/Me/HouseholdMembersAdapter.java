1c7cdbe6cbfc8e2d6b1977cd223d744d9ddcf9f0
Gjermund Bjaanes <bjaanes@gmail.com>
2014-10-10 20:20:57 +0200

    Continued clean up.

--- app/src/main/java/com/realkode/roomates/Me/HouseholdMembersAdapter.java
+++ app/src/main/java/com/realkode/roomates/Me/HouseholdMembersAdapter.java
@@ -10,55 +10,53 @@ import com.parse.ParseFile;
 import com.parse.ParseImageView;
 import com.parse.ParseQuery;
 import com.parse.ParseQueryAdapter;
-import com.parse.ParseUser;
 import com.realkode.roomates.ParseSubclassses.User;
 import com.realkode.roomates.R;
 
-/**
- * Adapter for listing household members
- */
 public class HouseholdMembersAdapter extends ParseQueryAdapter<User>  {
 
     public HouseholdMembersAdapter(Context context) {
 
         super(context, new ParseQueryAdapter.QueryFactory<User>() {
-            public ParseQuery create() {
-                ParseQuery query = new ParseQuery("_User");
-                query.whereEqualTo("activeHousehold", ParseUser.getCurrentUser().getParseObject("activeHousehold"));
+            public ParseQuery<User> create() {
+                ParseQuery<User> query = new ParseQuery<User>(User.class);
+                query.whereEqualTo("activeHousehold", User.getCurrentUser().getActiveHousehold());
                 query.orderByAscending("displayName");
                 query.setCachePolicy(ParseQuery.CachePolicy.CACHE_THEN_NETWORK);
+
                 return query;
             }
         });
     }
 
-    // Customize the layout by overriding getItemView
     @Override
     public View getItemView(User user, View view, ViewGroup parent) {
         if (view == null) {
             view = View.inflate(getContext(), R.layout.list_household_members_layout, null);
         }
-
         super.getItemView(user, view, parent);
 
-        // Add and download the image
-        ParseImageView profilePic = (ParseImageView) view.findViewById(R.id.icon);
-        ParseFile imageFile = user.getProfilePicture();
+        setProfilePicture(user, view);
+        setUpTitle(user, view);
 
-        if (imageFile != null) {
-            profilePic.setParseFile(imageFile);
-            profilePic.loadInBackground();
+        return view;
+    }
 
-        }
-
-
-        // Add the title view
-        TextView titleTextView = (TextView) view.findViewById(R.id.text1);
-        titleTextView.setText(user.getDisplayName());
-        titleTextView.setTextColor(Color.BLACK);
+    private void setUpTitle(User user, View view) {
+        TextView titleView = (TextView) view.findViewById(R.id.text1);
+        titleView.setText(user.getDisplayName());
+        titleView.setTextColor(Color.BLACK);
         TextView emailTextView = (TextView) view.findViewById(R.id.textViewUserEmail);
         emailTextView.setTextColor(Color.BLACK);
         emailTextView.setText(user.getEmail());
-        return view;
+    }
+
+    private void setProfilePicture(User user, View view) {
+        ParseImageView profilePic = (ParseImageView) view.findViewById(R.id.icon);
+        ParseFile imageFile = user.getProfilePicture();
+        if (imageFile != null) {
+            profilePic.setParseFile(imageFile);
+            profilePic.loadInBackground();
+        }
     }
 }
