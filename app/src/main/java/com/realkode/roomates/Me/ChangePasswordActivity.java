25512e703f11af9e7615d6a07a01dc121a06e17c
Gjermund Bjaanes <bjaanes@gmail.com>
2014-10-11 15:57:50 +0200

    Continued clean up. Changed out old progress bar with new progress dialog in change password.

--- app/src/main/java/com/realkode/roomates/Me/ChangePasswordActivity.java
+++ app/src/main/java/com/realkode/roomates/Me/ChangePasswordActivity.java
@@ -1,22 +1,11 @@
 package com.realkode.roomates.Me;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
-import com.parse.LogInCallback;
-import com.parse.ParseException;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
-import com.realkode.roomates.Helpers.InputValidation;
-import com.realkode.roomates.Helpers.ToastMaker;
-import com.realkode.roomates.ParseSubclassses.User;
 import com.realkode.roomates.R;
 
 
@@ -27,62 +16,19 @@ public class ChangePasswordActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_change_password);
 
-        final ProgressBar progressSpinner = (ProgressBar) findViewById(R.id.progressBar_ChangePassword);
-
-        final User user = User.getCurrentUser();
-        final String username = user.getUsername();
-        final EditText oldPassword = (EditText) findViewById(R.id.textOldPassword);
-        final EditText newPassword = (EditText) findViewById(R.id.textNewPassword);
-        final EditText repeatPassword = (EditText) findViewById(R.id.textRepeatPassword);
+        final EditText oldPasswordField = (EditText) findViewById(R.id.textOldPassword);
+        final EditText newPasswordField = (EditText) findViewById(R.id.textNewPassword);
+        final EditText repeatPasswordField = (EditText) findViewById(R.id.textRepeatPassword);
 
         Button changePasswordButton = (Button) findViewById(R.id.buttonChangePwd);
-        changePasswordButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                if (newPassword.getText().toString().equals(repeatPassword.getText().toString())) {
-
-                    // Check password strength
-                    if (InputValidation.passwordIsValid(newPassword.getText().toString())) {
-                        progressSpinner.setVisibility(View.VISIBLE);
-                        // Trying to log in to check if old password is correct
-                        ParseUser.logInInBackground(username, oldPassword.getText().toString(), new LogInCallback() {
-                            @Override
-                            public void done(ParseUser user2, ParseException arg1) {
-                                progressSpinner.setVisibility(View.INVISIBLE);
-                                if (user2 != null)
-                                    changePassword(newPassword.getText().toString());
-                                else
-                                    ToastMaker.makeLongToast("You entered the wrong password", getApplicationContext());
-                            }
-                        });
-                    } else {
-                        ToastMaker.makeLongToast("Password is not valid. " +
-                                "A Valid password needs to be at least 6 characters long, " +
-                                "have at least one upper and one lower case letter " +
-                                "and at least one number", getApplicationContext());
-                    }
-                } else {
-                    ToastMaker.makeLongToast("Passwords do not match", getApplicationContext());
-                }
-            }
-        });
+        changePasswordButton.setOnClickListener(
+                new ChangePasswordOnClickListener(oldPasswordField, newPasswordField, repeatPasswordField, this)
+        );
     }
 
-    private void changePassword(String newPwd) {
-        ParseUser.getCurrentUser().setPassword(newPwd);
-        ParseUser.getCurrentUser().saveInBackground(new SaveCallback() {
 
-            public void done(ParseException e) {
-                if (e == null)
-                    ToastMaker.makeLongToast("Password changed", getApplicationContext());
-                else
-                    ToastMaker.makeLongToast(e.getMessage(), getApplicationContext());
-            }
-        });
-    }
 }
---
+++ app/src/main/java/com/realkode/roomates/Me/ChangePasswordOnClickListener.java
@@ -0,0 +1,95 @@
+package com.realkode.roomates.Me;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.view.View;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.realkode.roomates.Helpers.InputValidation;
+import com.realkode.roomates.Helpers.ToastMaker;
+import com.realkode.roomates.ParseSubclassses.User;
+import com.realkode.roomates.R;
+
+public class ChangePasswordOnClickListener implements View.OnClickListener {
+    private EditText oldPasswordField;
+    private EditText newPasswordField;
+    private EditText repeatPasswordField;
+    private Activity activity;
+    private Context context;
+
+    ChangePasswordOnClickListener(EditText oldPasswordField,
+                                  EditText newPasswordField,
+                                  EditText repeatPasswordField,
+                                  Activity activity)
+    {
+        this.oldPasswordField = oldPasswordField;
+        this.newPasswordField = newPasswordField;
+        this.repeatPasswordField = repeatPasswordField;
+        this.activity = activity;
+        this.context = activity;
+    }
+
+    @Override
+    public void onClick(View v) {
+        final String newPassword = newPasswordField.getText().toString();
+        final String oldPassword = oldPasswordField.getText().toString();
+        final String repeatPassword = repeatPasswordField.getText().toString();
+        final String username = User.getCurrentUser().getUsername();
+
+        if (newPassword.equals(repeatPassword)) {
+
+            if (InputValidation.passwordIsValid(newPassword)) {
+                final ProgressDialog loginProgress = ProgressDialog.show(context,
+                        context.getString(R.string.progress_bar_change_password_login_title),
+                        context.getString(R.string.progress_bar_change_password_login_message),
+                        true);
+
+                // Log in to check if old password is correct
+                ParseUser.logInInBackground(username, oldPassword, new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException exception) {
+                        loginProgress.dismiss();
+                        if (user != null)
+                            changePassword(newPassword);
+                        else
+                            ToastMaker.makeLongToast(R.string.toast_change_password_wrong_password, context);
+                    }
+                });
+            } else {
+                ToastMaker.makeLongToast(R.string.toast_change_password_invalid, context);
+            }
+        } else {
+            ToastMaker.makeLongToast(R.string.toast_change_password_dont_match, context);
+        }
+
+
+    }
+
+    private void changePassword(String newPassword) {
+        ParseUser.getCurrentUser().setPassword(newPassword);
+
+
+        final ProgressDialog changeProgress = ProgressDialog.show(context,
+                context.getString(R.string.progress_dialog_change_password_title),
+                context.getString(R.string.progress_dialog_change_password_message),
+                true);
+        ParseUser.getCurrentUser().saveInBackground(new SaveCallback() {
+
+            public void done(ParseException exception) {
+                changeProgress.dismiss();
+                if (exception == null) {
+                    ToastMaker.makeLongToast(R.string.toast_change_password_success, context);
+                    activity.finish();
+                }
+                else {
+                    ToastMaker.makeLongToast(exception.getMessage(), context);
+                }
+            }
+        });
+    }
+}
